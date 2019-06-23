d3e7187b5a25cc33ec43688dcfd01f2e7aaca1a6
tavisca-ksingh <51925469+tavisca-ksingh@users.noreply.github.com>
2019-06-23 09:46:40 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -4,9 +4,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
     {
-       static void Main(string[] args)
+        
+        static void Main(string[] args)
         {
-            Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
+            Test(new[] {"22:12:12"}, new [] { "10 hours ago" }, "08:12:12");
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
             Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
             Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
@@ -52,8 +53,13 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                         else if(showPostTime[i].Contains("hours"))
                         {
                             string hour = showPostTime[i].Split(" ")[0];
-                            TimeSpan ts = new TimeSpan(Int32.Parse(hour),0,0);                            
-                            resString[i] = dt.Add(ts).ToString().Split(" ")[1];
+                            TimeSpan ts = new TimeSpan(Int32.Parse(hour),0,0);    
+                            DateTime ndt = dt.Add(ts);
+                            string s = ndt.ToString("HH:mm:ss");
+                            resString[i] =s;
+                        }
+                        else{
+                            resString[i] = "error";
                         }
                         
                     }
