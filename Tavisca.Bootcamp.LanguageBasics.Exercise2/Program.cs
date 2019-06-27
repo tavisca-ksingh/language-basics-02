5bd3087bf7dfbddf151a2c361e2cb3ed9aefa27f
tavisca-ksingh <51925469+tavisca-ksingh@users.noreply.github.com>
2019-06-27 12:21:45 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 
-namespace src
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
